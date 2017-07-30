2263868b640181ed884775b35a7dba94c5adab2c
Michele Beccalossi <beccalossi.michele@gmail.com>
2017-07-31 01:11:42 +0200

    build_kernel: use custom kernel name and version
    Signed-off-by: Michele Beccalossi <beccalossi.michele@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+KERNEL_NAME="primal.grace"
+KERNEL_VERSION="1.0.0"
+KERNEL_REVISION="0"
+KERNEL_BETA="1"
+
+if [ $KERNEL_BETA == 1 ]; then
+	KERNEL_VERSION+=β
+fi
+if [ $KERNEL_REVISION != 0 ]; then
+	KERNEL_VERSION+=-r$KERNEL_REVISION
+fi
+export LOCALVERSION=-${KERNEL_NAME}-v${KERNEL_VERSION}
+
 export ARCH=arm64
 export CROSS_COMPILE=../gcc-linaro-5.4.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 
-make exynos8890-hero2lte_defconfig
+make primal-hero2lteeur_defconfig
 make -j4 2>&1 | tee -a ./build_kernel.log
