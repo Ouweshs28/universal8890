2ed6b010fe1f281242b6df5a7153dd033ceda8f6
Michele Beccalossi <beccalossi.michele@gmail.com>
2017-07-31 01:11:42 +0200

    build_kernel: use custom kernel name and version
    Signed-off-by: Michele Beccalossi <beccalossi.michele@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+KERNEL_NAME="Primal_Kernel"
+KERNEL_VERSION="2.5.0"
+KERNEL_REVISION="0"
+KERNEL_BETA="1"
+
+if [ $KERNEL_BETA == 1 ]; then
+	KERNEL_VERSION+=β
+fi
+if [ $KERNEL_REVISION != 0 ]; then
+	KERNEL_VERSION+=-r$KERNEL_REVISION
+fi
+export LOCALVERSION=-${KERNEL_NAME}-v${KERNEL_VERSION}
+
 export ARCH=arm64
 export CROSS_COMPILE=../aarch64-linux-android-4.9/bin/aarch64-linux-android-
 
