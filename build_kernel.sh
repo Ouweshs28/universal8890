d6ed7345ce2e3b7163f3b177dca960fe8afe7683
Michele Beccalossi <beccalossi.michele@gmail.com>
2017-07-31 01:11:42 +0200

    build_kernel: use custom kernel name and version
    Signed-off-by: Michele Beccalossi <beccalossi.michele@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+KERNEL_NAME="Prophecy_Kernel"
+KERNEL_VERSION="1.0.0"
+KERNEL_REVISION="0"
+KERNEL_BETA="1"
+
+if [ $KERNEL_BETA == 1 ]; then
+	KERNEL_VERSION+=β
+fi
+if [ $KERNEL_REVISION != 0 ]; then
+	KERNEL_VERSION+=-r$KERNEL_REVISION
+fi
+export LOCALVERSION=-${KERNEL_NAME}-v${KERNEL_VERSION}
+
 export ARCH=arm64
 export CROSS_COMPILE=../aarch64-linux-android-4.9/bin/aarch64-linux-android-
 
-make exynos8890-hero2lte_defconfig
+make prophecy-hero2lteeur_defconfig
 make -j4 2>&1 | tee -a ./build_kernel.log
