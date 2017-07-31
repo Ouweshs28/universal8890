0b2f86e46abb74d69634e7685fdd6c132831aff4
Michele Beccalossi <beccalossi.michele@gmail.com>
2017-07-31 15:30:34 +0200

    ramdisk: set up services and add init.d support
    Signed-off-by: Michele Beccalossi <beccalossi.michele@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -339,7 +339,7 @@ FUNC_BUILD_ZIP()
 	cd $ZIPDIR
 	echo "=> Output: $EXTDIR/${KERNEL_NAME}-v${KERNEL_VERSION}${BUILD_TYPE}-${MODEL}${VARIANT}.zip"
 	echo ""
-	zip -r9 $EXTDIR/${KERNEL_NAME}-v${KERNEL_VERSION}${BUILD_TYPE}-${MODEL}${VARIANT}.zip * .version -x modules/\* -x ramdisk/\*
+	zip -r9 $EXTDIR/${KERNEL_NAME}-v${KERNEL_VERSION}${BUILD_TYPE}-${MODEL}${VARIANT}.zip * .version -x modules/\*
 
 	echo ""
 	echo "=================================================================="
